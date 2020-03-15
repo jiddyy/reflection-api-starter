a78230be3733ed43087f67f23202e91bda1d9f67
Nate Clark <natec425@gmail.com>
2020-03-15 17:17:22 -0500

    Generate project from Spring Initializer

---
+++ src/main/java/org/basecampcodingacademy/reflections/ReflectionsApplication.java
@@ -0,0 +1,13 @@
+package org.basecampcodingacademy.reflections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReflectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/basecampcodingacademy/reflections/ReflectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.basecampcodingacademy.reflections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
